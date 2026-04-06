5a5e4c82d717f2bd93d922652efbe43e43cb667c
Lucas Eduardo <carpa135@gmail.com>
2026-04-06 14:31:51 -0300

    atualizei o entity.ts

--- entity.ts
+++ entity.ts
@@ -1,151 +1,125 @@
-export abstract class Usuario {
-  constructor(
-    public idUsuario: number,
-    public nome: string,
-    public email: string,
-    public senha: string,
-    public telefone: string,
-    public tipoUsuario: string,
-  ) {}
+class Usuario {
+    constructor(
+        public idUsuario: number,
+        public nome: string,
+        public email: string,
+        public senha: string,
+        public telefone: string,
+        public tipoUsuario: string
+    ) {}
 }
 
-export class Cliente extends Usuario {
-  constructor(
-    idUsuario: number,
-    nome: string,
-    email: string,
-    senha: string,
-    telefone: string,
-    tipoUsuario: string,
-    public idCliente: number,
-    public cpf: string,
-    public endereco: string,
-  ) {
-    super(idUsuario, nome, email, senha, telefone, tipoUsuario);
-  }
+class Cliente extends Usuario {
+    public pedidos: Pedido[] = [];
+    public carrinho: Carrinho | null = null;
+
+    constructor(
+        idUsuario: number,
+        nome: string,
+        email: string,
+        senha: string,
+        telefone: string,
+        tipoUsuario: string,
+        public idCliente: number,
+        public cpf: string,
+        public endereco: string
+    ) {
+        super(idUsuario, nome, email, senha, telefone, tipoUsuario);
+    }
 }
 
-export class Produtor extends Usuario {
-  public produtos: Produto[] = [];
+class Produtor extends Usuario {
+    public produtos: Produto[] = [];
 
-  constructor(
-    idUsuario: number,
-    nome: string,
-    email: string,
-    senha: string,
-    telefone: string,
-    tipoUsuario: string,
-    public idProdutor: number,
-    public nomePropriedade: string,
-    public cnpjCpf: string,
-    public localizacao: string,
-    public descricao: string,
-  ) {
-    super(idUsuario, nome, email, senha, telefone, tipoUsuario);
-  }
-
-  cadastrarProduto(produto: Produto): void {
-    this.produtos.push(produto);
-  }
+    constructor(
+        idUsuario: number,
+        nome: string,
+        email: string,
+        senha: string,
+        telefone: string,
+        tipoUsuario: string,
+        public idProdutor: number,
+        public nomePropriedade: string,
+        public cnpjCpf: string,
+        public localizacao: string,
+        public descricao: string
+    ) {
+        super(idUsuario, nome, email, senha, telefone, tipoUsuario);
+    }
 }
 
-export class Produto {
-  constructor(
-    public idProduto: number,
-    public nome: string,
-    public descricao: string,
-    public preco: number,
-    public estoque: number,
-    public categoria: string,
-    public produtor: Produtor,
-  ) {}
+class Produto {
+    constructor(
+        public idProduto: number,
+        public nome: string,
+        public descricao: string,
+        public preco: number,
+        public estoque: number,
+        public categoria: string,
+        public produtor: Produtor
+    ) {}
 }
 
-export class ItemCarrinho {
-  constructor(
-    public idItemCarrinho: number,
-    public produto: Produto,
-    public quantidade: number,
-  ) {}
-
-  get subtotal(): number {
-    return this.produto.preco * this.quantidade;
-  }
+class ItemCarrinho {
+    constructor(
+        public idItemCarrinho: number,
+        public quantidade: number,
+        public subtotal: number,
+        public produto: Produto
+    ) {}
 }
 
-export class Carrinho {
-  public itens: ItemCarrinho[] = [];
+class Carrinho {
+    public itens: ItemCarrinho[] = [];
 
-  constructor(
-    public idCarrinho: number,
-    public dataCriacao: Date,
-    public cliente: Cliente,
-  ) {}
-
-  adicionarItem(item: ItemCarrinho): void {
-    this.itens.push(item);
-  }
-
-  removerItem(idItemCarrinho: number): void {
-    this.itens = this.itens.filter((item) => item.idItemCarrinho !== idItemCarrinho);
-  }
-
-  get valorTotal(): number {
-    return this.itens.reduce((total, item) => total + item.subtotal, 0);
-  }
+    constructor(
+        public idCarrinho: number,
+        public dataCriacao: Date,
+        public valorTotal: number,
+        public cliente: Cliente
+    ) {}
 }
 
-export class ItemPedido {
-  constructor(
-    public idItemPedido: number,
-    public produto: Produto,
-    public quantidade: number,
-    public precoUnitario: number,
-  ) {}
-
-  get subtotal(): number {
-    return this.precoUnitario * this.quantidade;
-  }
+class ItemPedido {
+    constructor(
+        public idItemPedido: number,
+        public quantidade: number,
+        public precoUnitario: number,
+        public subtotal: number,
+        public produto: Produto
+    ) {}
 }
 
-export class Pagamento {
-  constructor(
-    public idPagamento: number,
-    public tipoPagamento: string,
-    public statusPagamento: string,
-    public valor: number,
-    public dataPagamento: Date,
-  ) {}
+class Pagamento {
+    constructor(
+        public idPagamento: number,
+        public tipoPagamento: string,
+        public statusPagamento: string,
+        public valor: number,
+        public dataPagamento: Date
+    ) {}
 }
 
-export class Entrega {
-  constructor(
-    public idEntrega: number,
-    public enderecoEntrega: string,
-    public statusEntrega: string,
-    public previsaoEntrega: Date,
-    public taxaEntrega: number,
-  ) {}
+class Entrega {
+    constructor(
+        public idEntrega: number,
+        public enderecoEntrega: string,
+        public statusEntrega: string,
+        public previsaoEntrega: Date,
+        public taxaEntrega: number
+    ) {}
 }
 
-export class Pedido {
-  public itens: ItemPedido[] = [];
+class Pedido {
+    public itens: ItemPedido[] = [];
 
-  constructor(
-    public idPedido: number,
-    public dataPedido: Date,
-    public status: string,
-    public cliente: Cliente,
-    public pagamento: Pagamento,
-    public entrega: Entrega,
-  ) {}
-
-  adicionarItem(item: ItemPedido): void {
-    this.itens.push(item);
-  }
-
-  get valorTotal(): number {
-    const totalItens = this.itens.reduce((total, item) => total + item.subtotal, 0);
-    return totalItens + this.entrega.taxaEntrega;
-  }
+    constructor(
+        public idPedido: number,
+        public dataPedido: Date,
+        public status: string,
+        public valorTotal: number,
+        public cliente: Cliente,
+        public pagamento: Pagamento,
+        public entrega: Entrega
+    ) {}
 }
